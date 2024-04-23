0bc1a61fb8e50f4a454e0425e196cfd7d31f86e2
ayushkumar32539 <ayush2010128@akgec.ac.in>
2024-04-24 00:53:10 +0530

    added

--- src/App.js
+++ src/App.js
@@ -8,11 +8,11 @@ import Footer from './Components/Footer';
 function App() {
   return (
     <Router>
-       <Navbar />
-       <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        </Routes>    
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
       <Footer />
     </Router>
   );
